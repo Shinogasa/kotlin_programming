0a7d56a5f3182522e01bab1793b001329a039c84
Shinonome <cuulbulu@gmail.com>
2024-09-19 17:33:30 +0900

    Update: 14章終わり

---
+++ NyetHack/src/com/bignerdranch/nyethack/Tavern.kt
@@ -0,0 +1,77 @@
+package com.bignerdranch.nyethack
+
+import java.io.File
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
+val menuList = File("data/tavern-menu-data.txt")
+    .readText()
+    .split("\n")
+val patronGold = mutableMapOf<String, Double>()
+
+fun main(args: Array<String>) {
+    (0..9).forEach {
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+
+    uniquePatrons.forEach {
+        patronGold[it] = 6.0
+    }
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        orderCount++
+    }
+
+    displayPatronBalances()
+}
+
+private fun displayPatronBalances() {
+    patronGold.forEach { patron, balance ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
+    }
+}
+
+fun performPurchase(price: Double, patronName: String) {
+    val totalPurse = patronGold.getValue(patronName)
+    patronGold[patronName] = totalPurse - price
+}
+
+private fun toDragonSpeak(phrase: String): String =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value) {
+            "a", "A" -> "4"
+            "e", "E" -> "3"
+            "i", "I" -> "1"
+            "o", "O" -> "0"
+            "u", "U" -> "|_|"
+            else -> it.value
+        }
+}
+
+private fun placeOrder(patronName: String, menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("$patronName speaks with the ${tavernMaster} about their order.")
+
+    val (type, name, price) =  menuData.split(',')
+    val message = "$patronName buys a $name ($type) for $price."
+    println(message)
+
+    performPurchase(price.toDouble(), patronName)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "$patronName exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+
+    } else {
+        "$patronName says: Thanks for the $name."
+    }
+    println(phrase)
+}
