19d3cb5b49351d051b213011f5aedb9614b9a530
Shinonome <cuulbulu@gmail.com>
2024-09-20 10:12:31 +0900

    Update: 15章終わり

--- NyetHack/src/com/bignerdranch/nyethack/Game.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Game.kt
@@ -1,20 +1,78 @@
 package com.bignerdranch.nyethack
 
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
-    val player = Player("Madrigal")
-    player.castFireball()
-
-    var currentRoom: Room = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    printPlayerStatus(player)
+    Game.play()
 }
 
-private fun printPlayerStatus(
-    player: Player
-) {
-    println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus()}")
+object Game {
+    private val player = Player("Madrigal")
+    private var currentRoom: Room = TownSquare()
+
+    private var woldMap = listOf(
+        listOf(currentRoom, Room("Tavern"), Room("Back Room")),
+        listOf(Room("Long Corridor"), Room("Generic Room"))
+    )
+
+    private fun move(directionInput: String) = try {
+        val direction = Direction.valueOf(directionInput.toUpperCase())
+        val newPosition = direction.updateCoordinate(player.currentPosition)
+        if (!newPosition.isInBounds) {
+            throw IllegalStateException("$direction is out of bounds.")
+        }
+        val newRoom = woldMap[newPosition.y][newPosition.x]
+        player.currentPosition = newPosition
+        currentRoom = newRoom
+        "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
+    } catch (e: Exception) {
+        "Invalid direction: $directionInput."
+    }
+
+    init {
+        println("Welcome, adventurer.")
+        player.castFireball()
+    }
+
+    fun play() {
+        while (true) {
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+        }
+    }
+
+    private fun exit() {
+        println("Farewell message to adventurer.")
+        exitProcess(0)
+    }
+
+    private fun printPlayerStatus(
+        player: Player
+    ) {
+        println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, { "" })
+
+        fun processCommand() = when(command.toLowerCase()) {
+            "move" -> move(argument)
+            "quit", "exit" -> exit()
+            else -> commandNotFound()
+        }
+
+        private fun commandNotFound() {
+            println("I'm not quite sure what you're trying to do!")
+        }
+    }
+
 }
 
